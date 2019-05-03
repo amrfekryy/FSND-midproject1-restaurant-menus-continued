6c0eee7cba5072cb72a44960d4d3290dd99d929f
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2019-05-03 19:41:09 +0200

    Enable closing flash messages

---
+++ static/scripts.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // flash message close button
+  document.querySelector('#flash-close').onclick = () => {
+    document.querySelector('#flash').style.display = "none";
+  }
+
+
+});
