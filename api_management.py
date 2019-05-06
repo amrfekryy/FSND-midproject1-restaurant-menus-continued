7888f53808bd1716f9e276861a94e1b8be695848
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2019-05-06 17:53:16 +0200

    Enable PUT, DELETE actions on a retaurant via API

--- api_management.py
+++ api_management.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append("./database/")
 
 # flask
-from flask import (Blueprint, render_template, jsonify)
+from flask import (Blueprint, render_template, request, jsonify)
 # general
 from helpers import *
 # database
@@ -17,34 +17,67 @@ api_management = Blueprint('api_management', __name__,
                      static_folder='static')
 
 
+# API documnetation
 @api_management.route('/api/')
 def api():
     user = get_user(login_session.get('user_id'))
     return render_template('api.html', user=user)
 
 
+# GET all restaurants
 @api_management.route('/api/restaurants/')
-@login_required
 def api_restaurants():
     restaurants_list = session.query(Restaurant).all()    
     return jsonify({'restaurants':[restaurant.serialize for restaurant in restaurants_list]})
 
 
-@api_management.route('/api/restaurant/<int:restaurant_id>/')
-@login_required
+# GET/ PUT/DELETE an existing restaurant
+@api_management.route('/api/restaurant/<int:restaurant_id>/', methods=['GET', 'PUT', 'DELETE'])
 def api_restaurant(restaurant_id):
+    # verify id exists in database
     try:
         restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     except:
-        return f"There is no restaurant with id = {restaurant_id}"
+        return jsonify({
+            'success':False, 
+            'description': f'There is no restaurant with id:{restaurant_id}'
+            }), 400
 
-    restaurant_dict = restaurant.serialize
-    restaurant_dict['restaurant_menu'] = [item.serialize for item in restaurant.menu_items]
-    return jsonify(restaurant_dict)
+    if request.method == 'GET':
+        restaurant_dict = restaurant.serialize
+        restaurant_dict['restaurant_menu'] = [item.serialize for item in restaurant.menu_items]
+        return jsonify(restaurant_dict)
+
+    elif request.method == 'PUT':
+        restaurant_new_name = request.args.get('name')
+
+        if restaurant_new_name:
+            restaurant.name = restaurant_new_name
+            session.add(restaurant)
+            session.commit()
+            return jsonify({
+                'success':True, 
+                'description': f'Restaurant with id:{restaurant_id} has been updated'
+                }), 200
+        else:
+            return jsonify({
+                'success':False, 
+                'description': "couldn't collect 'name' parameter"
+                }), 400
+
+    elif request.method == 'DELETE':
+        for item in restaurant.menu_items:
+            session.delete(item)
+        session.delete(restaurant)
+        session.commit()
+        return jsonify({
+            'success':True, 
+            'description': f'Restaurant with id:{restaurant_id} has been deleted'
+            }), 200
 
 
+# GET/ PUT/DELETE an existing menu item
 @api_management.route('/api/menu_item/<int:item_id>/')
-@login_required
 def api_menu_item(item_id):
     try:
         menu_item = session.query(MenuItem).filter_by(id=item_id).one()
@@ -61,8 +94,8 @@ def api_menu_item(item_id):
     return jsonify(item_dict)
 
 
+# GET all data
 @api_management.route('/api/all/')
-@login_required
 def api_all():
     restaurants_list = session.query(Restaurant).all()
     all_data = []
--- app.py
+++ app.py
@@ -272,6 +272,6 @@ def worldwide_mashup():
 
 if __name__ == '__main__':
     # app.secret_key = "super_secret_key"
-    # app.debug = True
+    app.debug = True
     # app.run(host='0.0.0.0', port=5000)
     app.run()
